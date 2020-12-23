aa7e749bdd24e4d525b0a02f1914c1eb34591409
gunhee lee <dlrjsgml42@naver.com>
2020-12-23 18:40:20 +0900

    feat: 프로젝트 생성
    - 프로젝트 생성, 스프링 부트 initializer를 이용해 gradle 기반으로 생성
    - gitignore를 통해 빌드 파일 제거
    - 패키지명 정리

---
+++ src/main/java/com/booksdiary/booksdiary/SpringBooksDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.booksdiary.booksdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBooksDiaryApplication {
+    // TODO: 2020/12/23 패키지명 명확히 정리하기
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBooksDiaryApplication.class, args);
+    }
+
+}
