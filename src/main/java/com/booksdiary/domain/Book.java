2fdf838e8436324ba9672f0dd3477d8171953093
gunhee lee <dlrjsgml42@naver.com>
2021-01-26 16:34:48 +0900

    feat: 잘못된 도메인 코드 수정
    - Book 코드의 오타 및 잘못된 내용 수정

--- src/main/java/com/booksdiary/domain/Book.java
+++ src/main/java/com/booksdiary/domain/Book.java
@@ -18,9 +18,10 @@ import javax.persistence.Id;
 @Entity
 // TODO: 2021/01/25 아직 비즈니스 로직이 없다!! 추후 테스트코드 작성
 public class Book {
-    String name;
     // TODO: 2021/01/25 JPA 책 바탕으로 ID부분 왜 저렇게 썼나 게시!!
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
 }
