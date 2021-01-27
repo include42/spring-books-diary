8a4df56b9d83c25c97b2b0860c153382facfdb4c
gunhee lee <dlrjsgml42@naver.com>
2021-01-27 12:57:42 +0900

    feat: 도메인 이름을 복수형으로 변경, 학습 테스트 삭제
    - 도메인 Book의 이름을 Books로 바꾸고, 이에 맞게 저장소 이름 수정
    - 학습을 위해 만든 BookRepositoryTest를 삭제

--- src/main/java/com/booksdiary/domain/Books.java
+++ src/main/java/com/booksdiary/domain/Books.java
@@ -17,7 +17,7 @@ import javax.persistence.Id;
 // TODO: 2021/01/25 추후 createdDate 작업할때 AuditingEntityLiester
 @Entity
 // TODO: 2021/01/25 아직 비즈니스 로직이 없다!! 추후 테스트코드 작성
-public class Book {
+public class Books {
     // TODO: 2021/01/25 JPA 책 바탕으로 ID부분 왜 저렇게 썼나 게시!!
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/booksdiary/domain/BooksRepository.java
+++ src/main/java/com/booksdiary/domain/BooksRepository.java
@@ -2,5 +2,5 @@ package com.booksdiary.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BooksRepository extends JpaRepository<Books, Long> {
 }
