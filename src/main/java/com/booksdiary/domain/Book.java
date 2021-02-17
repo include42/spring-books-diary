db82e43592d32c4da2fb4929befe131f2d12e08c
gunhee lee <dlrjsgml42@naver.com>
2021-02-17 11:54:51 +0900

    feat: Book 엔티티 클래스를 설계에 맞게 다시 구현
    - Book 클래스를 ERD 및 기능 목록에 맞게 재설계
    - Book 클래스의 필드에 validation 적용
    - CreateDate 옵션을 사용할 수 있도록 JpaAuditingConfig 추가

--- src/main/java/com/booksdiary/domain/Book.java
+++ src/main/java/com/booksdiary/domain/Book.java
@@ -1,24 +1,56 @@
 package com.booksdiary.domain;
 
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.time.LocalDateTime;
 
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-// TODO: 2021/01/25 추후 createdDate 작업할때 AuditingEntityLiester
+@EntityListeners(AuditingEntityListener.class)
 @Entity
-// TODO: 2021/01/25 아직 비즈니스 로직이 없다!! 추후 테스트코드 작성
 public class Book {
-    // TODO: 2021/01/25 JPA 책 바탕으로 ID부분 왜 저렇게 썼나 게시!!
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @NotBlank
+    private String isbn;
+
+    @NotBlank
     private String name;
+
+    @NotBlank
+    private String content;
+
+    @NotNull
+    private LocalDateTime publishDate;
+
+    @NotBlank
+    private String publisher;
+
+    @NotNull
+    private String author;
+    // TODO: 2021/02/17 저자가 여러 명일때, 일단 지금은 쉼표 표함해서 받음. 추후 리스트화하는 거 고민
+
+    private String translator;
+
+    @Positive
+    private Long price;
+
+    @NotBlank
+    private String thumbnail;
+
+    @Positive
+    private Long page;
 }
