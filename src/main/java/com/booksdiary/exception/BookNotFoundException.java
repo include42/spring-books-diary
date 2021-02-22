578bc5dbd621c8da8b7d246c8f7c6f6dd663ed38
gunhee lee <dlrjsgml42@naver.com>
2021-02-22 19:36:19 +0900

    feat: Exception 클래스의 상속구조 재설계
    - RuntimeException을 상속받는 BusinessException을 만들고, Custom Exception은 모두 이 클래스를 상속하도록 하였음
    - BusinessException을 상속받은 EntityNotFoundException을 구현. 엔티티를 찾지 못할 경우 이 예외의 상속 구현체를 사용함
    - BusinessException을 상속받은 InvalidValueException을 구현. 잘못된 값이 들어가 예외가 발생할 경우 이 예외의 상속 구현체를 사용함
    - EntityNotFoundException을 상속받는 것으로 BookNotFoundException을 재설계

--- src/main/java/com/booksdiary/exception/BookNotFoundException.java
+++ src/main/java/com/booksdiary/exception/BookNotFoundException.java
@@ -1,6 +1,6 @@
 package com.booksdiary.exception;
 
-public class BookNotFoundException extends RuntimeException {
+public class BookNotFoundException extends EntityNotFoundException {
     public BookNotFoundException(Long bookId) {
         super(bookId + "에 해당하는 도서를 찾을 수 없습니다.");
     }
