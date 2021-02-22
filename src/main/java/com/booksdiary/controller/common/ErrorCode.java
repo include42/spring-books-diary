c96722ecaa3e2c2dd9cbfa782e97b083d48a72ad
gunhee lee <dlrjsgml42@naver.com>
2021-02-22 19:56:52 +0900

    feat: ErrorCode와 ErrorResponse 구현
    - ErrorCode Enum class 구현, 이를 통해 에러의 케이스들을 정리하고 프론트엔드에서 받도록 한다.
    - ErrorResponse 구현. 이를 통해 Error나 Exception 발생 시 상태를 전달한다.

---
+++ src/main/java/com/booksdiary/controller/common/ErrorCode.java
@@ -0,0 +1,29 @@
+package com.booksdiary.controller.common;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+
+@Getter
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum ErrorCode {
+
+    // Common
+    INVALID_INPUT_VALUE(400, "C001", " Invalid Input Value"),
+    METHOD_NOT_ALLOWED(405, "C002", " Invalid Input Value"),
+    ENTITY_NOT_FOUND(400, "C003", " Entity Not Found"),
+    INTERNAL_SERVER_ERROR(500, "C004", "Server Error"),
+    INVALID_TYPE_VALUE(400, "C005", " Invalid Type Value"),
+    HANDLE_ACCESS_DENIED(403, "C006", "Access is Denied");
+
+    // TODO: 2021/02/22 여기에서 로그인, 도메인, 기타 등등의 예외 코드를 추가할 수 있다.
+
+    private final String code;
+    private final String message;
+    private final int status;
+
+    ErrorCode(final int status, final String code, final String message) {
+        this.status = status;
+        this.message = message;
+        this.code = code;
+    }
+}
