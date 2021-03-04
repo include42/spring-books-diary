1e0b631693114245f18476c61a12ebf7ea4706f4
gunhee lee <dlrjsgml42@naver.com>
2021-03-04 17:40:31 +0900

    refactor: GlobalExceptionHandler의 로그 오타 수정

--- src/main/java/com/booksdiary/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/booksdiary/global/error/GlobalExceptionHandler.java
@@ -105,7 +105,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ErrorResponse> handleException(Exception e) {
-        log.error("handleEntityNotFoundException", e);
+        log.error("Exception", e);
         final ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
