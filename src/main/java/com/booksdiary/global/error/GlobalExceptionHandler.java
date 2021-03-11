8f3dae05dc0478bc86b2fa5252657a185e3e8555
gunhee lee <dlrjsgml42@naver.com>
2021-03-11 12:43:29 +0900

    refactor: 코드 포맷 수정 및 문서화 주석 GlobalExceptionHandler에 작성

--- src/main/java/com/booksdiary/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/booksdiary/global/error/GlobalExceptionHandler.java
@@ -21,17 +21,20 @@ import java.nio.file.AccessDeniedException;
  * Exception을 상속받은 모든 클래스에 대해 대응하며, ErrorResponse를 ResponseEntity에 담아 반환한다.
  * <p>
  * ref https://cheese10yun.github.io/spring-guide-exception/
+ *
+ * @Author Geonhee lee
  */
 @Slf4j(topic = "ERROR_FILE_LOGGER")
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    // TODO: 2021/03/05 문서화 주석 마저 달기
     // TODO: 2021/03/05 예외 클래스 정보와 에러 메시지를 얼마정도 전달해줘야 할까? 고민해보기
 
     /**
-     * javax.validation.Valid or @Validated 으로 binding error 발생시 발생한다.
-     * HttpMessageConverter 에서 등록한 HttpMessageConverter binding 못할경우 발생
-     * 주로 @RequestBody, @RequestPart 어노테이션에서 발생
+     * handleMethodArgumentNotValidException메서드는 javax.validation.Valid or @Validated 으로 binding error 발생시 발생한다.
+     * 주로 @RequestBody, @RequestPart 어노테이션에서 발생한다.
+     *
+     * @param e 전달받은 MethodArgumentNotValidException 예외 객체이다.
+     * @return ErrorResponse 객체를 ResponseEntity에 담아 반환한다.
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
@@ -41,8 +44,11 @@ public class GlobalExceptionHandler {
     }
 
     /**
-     * ModelAttribute 으로 binding error 발생시 BindException 발생한다.
+     * handleBindException은 ModelAttribute 으로 binding error가 발생할 시 호출된다.
      * ref https://docs.spring.io/spring/docs/current/spring-framework-reference/web.html#mvc-ann-modelattrib-method-args
+     *
+     * @param e 전달받은 BindException 예외 객체이다.
+     * @return ErrorResponse 객체를 ResponseEntity에 담아 반환한다.
      */
     @ExceptionHandler(BindException.class)
     protected ResponseEntity<ErrorResponse> handleBindException(BindException e) {
@@ -52,28 +58,39 @@ public class GlobalExceptionHandler {
     }
 
     /**
-     * enum type 일치하지 않아 binding 못할 경우 발생
-     * 주로 @RequestParam enum으로 binding 못했을 경우 발생
+     * handleMethodArgumentTypeMismatchException 메서드는 enum type이 일치하지 않아 binding을 못할 경우 호출된다.
+     * 주로 @RequestParam enum으로 binding 못했을 경우 발생한다.
+     *
+     * @param e 전달받은 MethodArgumentTypeMismatchException 예외 객체이다.
+     * @return ErrorResponse 객체를 ResponseEntity에 담아 반환한다.
      */
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    protected ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(
+            MethodArgumentTypeMismatchException e) {
         log.error("handleMethodArgumentTypeMismatchException", e);
         final ErrorResponse response = ErrorResponse.of(e);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     /**
-     * 지원하지 않은 HTTP method를 호출 할 경우 발생
+     * handleHttpRequestMethodNotSupportedException 메서드는 지원하지 않은 HTTP method를 호출 할 경우 발생한다.
+     *
+     * @param e 전달받은 HttpRequestMethodNotSupportedException 예외 객체이다.
+     * @return ErrorResponse 객체를 ResponseEntity에 담아 반환한다.
      */
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+    protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(
+            HttpRequestMethodNotSupportedException e) {
         log.error("handleHttpRequestMethodNotSupportedException", e);
         final ErrorResponse response = ErrorResponse.of(ErrorCode.METHOD_NOT_ALLOWED);
         return new ResponseEntity<>(response, HttpStatus.METHOD_NOT_ALLOWED);
     }
 
     /**
-     * Authentication 객체가 필요한 권한을 보유하지 않은 경우 발생합
+     * handleAccessDeniedException 메서드는 Authentication 객체가 필요한 권한을 보유하지 않은 경우 호출된다.
+     *
+     * @param e 전달받은 AccessDeniedException 예외 객체이다.
+     * @return ErrorResponse 객체를 ResponseEntity에 담아 반환한다.
      */
     @ExceptionHandler(AccessDeniedException.class)
     protected ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException e) {
@@ -82,22 +99,56 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, HttpStatus.valueOf(ErrorCode.HANDLE_ACCESS_DENIED.getStatus()));
     }
 
+    /**
+     * handleWebClientRequestException은 WebClient 요청 도중 예외 발생시 호출된다.
+     *
+     * @param e 전달받은 WebClientRequestException 예외 객체이다.
+     * @return ErrorResponse 객체를 ResponseEntity에 담아 반환한다.
+     */
+    @ExceptionHandler(WebClientRequestException.class)
+    protected ResponseEntity<ErrorResponse> handleWebClientRequestException(WebClientRequestException e) {
+        log.error("handleWebClientRequestException", e);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.WEBCLIENT_REQUEST_ERROR);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * handleEntityNotFoundException은 특정 엔티티를 조회/검색하는 도중 관련 예외 발생시 호출된다.
+     *
+     * @param e 전달받은 EntityNotFoundException 예외 객체이다.
+     * @return ErrorResponse 객체를 ResponseEntity에 담아 반환한다.
+     */
     @ExceptionHandler(EntityNotFoundException.class)
-    protected ResponseEntity<ErrorResponse> handleEntityNotFoundException(final BusinessException e) {
+    protected ResponseEntity<ErrorResponse> handleEntityNotFoundException(final EntityNotFoundException e) {
         log.error("handleEntityNotFoundException", e);
         final ErrorCode errorCode = e.getErrorCode();
         final ErrorResponse response = ErrorResponse.of(errorCode);
         return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatus()));
     }
 
+    /**
+     * handleInvalidValueException은 잘못된 값이 비즈니스 로직에서 도출되어 예외 발생시 호출된다.
+     *
+     * @param e 전달받은 InvalidValueException 예외 객체이다.
+     * @return ErrorResponse 객체를 ResponseEntity에 담아 반환한다.
+     */
     @ExceptionHandler(InvalidValueException.class)
-    protected ResponseEntity<ErrorResponse> handleInvalidValueException(final BusinessException e) {
+    protected ResponseEntity<ErrorResponse> handleInvalidValueException(final InvalidValueException e) {
         log.error("handleInvalidValueException", e);
         final ErrorCode errorCode = e.getErrorCode();
         final ErrorResponse response = ErrorResponse.of(errorCode);
         return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatus()));
     }
 
+    /**
+     * handleBusinessException 메서드는, 비즈니스 로직에서의 예외 발생 시 호출된다.
+     * InvalidValueException과 EntityNotFoundException은 BusinessException을 상속받아 구현한 예외 객체이다.
+     *
+     * @param e 전달받은 BusinessException 예외 객체이다.
+     * @return ErrorResponse 객체를 ResponseEntity에 담아 반환한다.
+     * @see InvalidValueException
+     * @see EntityNotFoundException
+     */
     @ExceptionHandler(BusinessException.class)
     protected ResponseEntity<ErrorResponse> handleBusinessException(final BusinessException e) {
         log.error("handleBusinessException", e);
@@ -106,13 +157,13 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatus()));
     }
 
-    @ExceptionHandler(WebClientRequestException.class)
-    protected ResponseEntity<ErrorResponse> handleWebClientRequestException(WebClientRequestException e) {
-        log.error("handleWebClientRequestException", e);
-        final ErrorResponse response = ErrorResponse.of(ErrorCode.WEBCLIENT_REQUEST_ERROR);
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
-
+    /**
+     * handleException 메서드는, 예외가 발생했는데 해당 예외에 대한 핸들링이 이루어지지 않을 경우 호출된다.
+     * 500 에러를 발생시키고, 해당 내용을 로깅 및 전달한다.
+     *
+     * @param e 전달받은 Exception 예외 객체이다.
+     * @return ErrorResponse 객체를 ResponseEntity에 담아 반환한다.
+     */
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ErrorResponse> handleException(Exception e) {
         log.error("Exception", e);
